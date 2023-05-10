75f70e631df994b397ae0c208d52df1649bc7216
Pavel Kurach <pavel.kurach1@gmail.com>
2023-05-10 16:05:57 +0200

    Add financial statement data models

--- src/utils/financial_data.py
+++ src/utils/financial_data.py
@@ -1,25 +1,30 @@
-import asyncio
+import datetime
+
 import aiohttp
 
+from src.models.financials import FinancialReportType
+from src.models.financials import ModelOfType
 from src.utils.config import FINANCIAL_MODELLING_PREP_API_KEY as FMP_KEY
-
-class FinancialReportType:
-    INCOME = 'income-statement'
-    BALANCE_SHEET = 'balance-sheet-statement'
-    CASH_FLOW = 'cash-flow-statement'
+from src.utils.config import FINANCIAL_MODELLING_PREP_URL as FMP_URL
 
 
-async def get_json_parsed_data(session: aiohttp.ClientSession, url: str):
+async def get_json_parsed_data(session: aiohttp.ClientSession,
+                               url: str) -> list[dict]:
     async with session.get(url) as response:
         data = await response.json()
         return data
 
 
-async def get_financial_report(session: aiohttp.ClientSession,
-                               report_type: FinancialReportType,
-                               ticker: str):
-    url = (f'https://financialmodelingprep'
-           f'.com/api/v3/{report_type}/{ticker}?apikey={FMP_KEY}')
-    financial_report = await get_json_parsed_data(session, url)
-    return financial_report
-
+async def get_financial_reports(session: aiohttp.ClientSession,
+                                report_type: FinancialReportType,
+                                ticker: str,
+                                years: dict[int]):
+    limit = datetime.date.today().year - min(years) + 1
+    url = f'{FMP_URL}/v3/{report_type}/{ticker}?limit={limit}&apikey={FMP_KEY}'
+    raw_reports = await get_json_parsed_data(session, url)
+    financial_reports = {}
+    for raw_report in raw_reports:
+        report = ModelOfType[report_type](**raw_report)
+        if report.dict()['date'].year in years:
+            financial_reports[report.dict()['date'].year] = report
+    return financial_reports
